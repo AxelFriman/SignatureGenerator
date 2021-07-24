30687802b4a47fc7f14f1fa1bae00d3c76299e21
Иван <prokvanya@yandex.ru>
2021-07-24 21:21:49 +0300

    Add project files.

---
+++ SignatureGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SignatureGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"theFile.txt";
+            int blockSize = 11;
+            int blockId = 0;
+            using (SHA256 mySHA256 = SHA256.Create())
+            using (FileStream fstream = File.OpenRead($"{path}"))
+            {
+                while ((blockId + 1) * blockSize <= fstream.Length)
+                {
+                    byte[] block = new byte[blockSize];
+                    fstream.ReadAsync(block, 0, blockSize);
+                    byte[] hash = mySHA256.ComputeHash(block);
+                    Console.WriteLine($"{blockId} - {BytesToString(hash)}");
+                    blockId++;
+                }
+            }
+        }
+
+        static string BytesToString(byte[] bytes)
+        {
+            StringBuilder str = new();
+            foreach (var b in bytes)
+            {
+                str.Append(String.Format("{0:X2}", b));
+            }
+            return str.ToString();
+        }
+    }
+}
