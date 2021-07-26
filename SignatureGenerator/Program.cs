4c6d74e76977c01070cded976291ec65c69a8166
Иван <prokvanya@yandex.ru>
2021-07-26 03:45:12 +0300

    доделал потоки, исключения, параметры

--- SignatureGenerator/Program.cs
+++ SignatureGenerator/Program.cs
@@ -11,20 +11,36 @@ namespace SignatureGenerator
     {
         static void Main(string[] args)
         {
-            //string path = @"theFile1.txt";
-            string path = @"F:/theFileL.txt";
-            int blockSize = 100_000_000;
+            string path = args[0];
+            int blockSize = 0;
             int blockId = 0;
-            List<Thread> threads = new List<Thread>();
-            
-            using (FileStream fstream = File.OpenRead($"{path}"))
+            try
             {
-                while ((blockId + 1) * (long) blockSize <= fstream.Length)
+                if (!File.Exists(path))
+                {
+                    throw new ArgumentException("File not found");
+                }
+                if (!Int32.TryParse(args[1], out blockSize) || blockSize == 0)
+                {
+                    throw new ArgumentException("Wrong block size.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.StackTrace);
+                return;
+            }
+
+            List<Thread> threads = new();
+
+            using (FileStream fstream = File.OpenRead(path))
+            {
+                while ((blockId + 1) * (long)blockSize <= fstream.Length)
                 {
                     var block = new BlockOfBytes(blockId, new byte[blockSize]);
-                    fstream.Read(block.data, 0, blockSize);
+                    fstream.Read(block.Data, 0, blockSize);
 
-                    Thread myThread = new Thread(new ParameterizedThreadStart(new Signature().AddSignature));
+                    Thread myThread = new Thread(new ParameterizedThreadStart(Signature.AddSignature)); 
 
                     threads.Add(myThread);
                     myThread.Start(block);
@@ -43,7 +59,7 @@ namespace SignatureGenerator
             Signature.PrintSignature();
         }
     }
-    public record BlockOfBytes(int id, byte[] data);
+    public record BlockOfBytes(int Id, byte[] Data);
     public class Signature
     {
         public static ConcurrentDictionary<int, string> Sign { get; set; } = new();
@@ -55,17 +71,24 @@ namespace SignatureGenerator
             }
 
         }
-        public void AddSignature(object blockObj)
+        public static void AddSignature(object blockObj)
         {
-            BlockOfBytes block = (BlockOfBytes)blockObj;
-            byte[] hash = new byte[1];
-            using (SHA256 mySHA256 = SHA256.Create())
+            try
             {
-                hash = mySHA256.ComputeHash(block.data);
+                BlockOfBytes block = (BlockOfBytes)blockObj;
+                byte[] hash = new byte[1];
+                using (SHA256 mySHA256 = SHA256.Create())
+                {
+                    hash = mySHA256.ComputeHash(block.Data);
+                }
+                int id = block.Id;
+                block = null;
+                Sign.TryAdd(id, BitConverter.ToString(hash).Replace("-", ""));
             }
-            int id = block.id;
-            block = null; 
-            Sign.TryAdd(id, BitConverter.ToString(hash).Replace("-", ""));
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.StackTrace);
+            }
         }
     }
 }
